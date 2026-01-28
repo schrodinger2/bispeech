0064439d3fbd180dee7480cb6ba4664e5a724024
saif <schrodinger08596@gmail.com>
2026-01-28 14:26:11 +0200

    start off

---
+++ rec.py
@@ -0,0 +1,37 @@
+import sounddevice as sd
+import numpy as np
+from scipy.io.wavfile import write
+import whisper
+
+SAMPLE_RATE = 16000
+DURATION = 15  # seconds
+
+print("Speak now...")
+audio = sd.rec(
+    int(DURATION * SAMPLE_RATE),
+    samplerate=SAMPLE_RATE,
+    channels=1,
+    dtype="float32"
+)
+sd.wait()
+print("Recording finished")
+i = 0
+src = f"audio/input{i}.wav"
+while(DURATION>i+3):
+    src = f"audio/input{i}.wav"
+    write(src, SAMPLE_RATE, audio[i* SAMPLE_RATE:(i+3) *SAMPLE_RATE])
+    i += 2
+
+src = f"audio/input{i}.wav"
+write(src, SAMPLE_RATE,audio[i* SAMPLE_RATE:-1])
+
+model = whisper.load_model("small")  
+
+for j in range(0,i,2):
+    result = model.transcribe(
+        f"audio/input{j}.wav"
+        # task="transcribe", 
+        # language=None,  # IMPORTANT: let it auto-detect
+        # condition_on_previous_text=False
+    )
+    print(result["text"])
